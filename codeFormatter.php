822138d4a5400ccbcc09b3e28867c05e8b398dc7
Carlos C <uldericofilho@gmail.com>
2014-07-09 15:54:04 +0200

    Update Code Formatter

--- codeFormatter.php
+++ codeFormatter.php
@@ -332,7 +332,13 @@ final class CodeFormatter {
 	}
 
 	public function formatCode($source = '') {
-		foreach ($this->passes as $pass) {
+		$passes = array_map(
+			function ($pass) {
+				return clone $pass;
+			},
+			$this->passes
+		);
+		while ($pass = array_shift($passes)) {
 			$source = $pass->format($source);
 		}
 		return $source;
@@ -941,6 +947,153 @@ final class ReindentColonBlocks extends FormatterPass {
 	}
 }
 
+final class ReindentLoopColonBlocks extends FormatterPass {
+	public function format($source) {
+		$source = $this->format_for_blocks($source);
+		$source = $this->format_foreach_blocks($source);
+		$source = $this->format_while_blocks($source);
+		return $source;
+	}
+
+	private function format_blocks($source, $open_token, $close_token) {
+		$this->tkns = token_get_all($source);
+		$this->code = '';
+
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr       = $index;
+			switch ($id) {
+				case $close_token:
+					$this->set_indent(-1);
+					$this->append_code($text, false);
+					break;
+				case $open_token:
+					$this->append_code($text, false);
+					while (list($index, $token) = each($this->tkns)) {
+						list($id, $text) = $this->get_token($token);
+						$this->ptr       = $index;
+						$this->append_code($text, false);
+						if (ST_CURLY_OPEN == $id) {
+							break;
+						} elseif (ST_COLON == $id && !$this->is_token(array(T_CLOSE_TAG))) {
+							$this->set_indent(+1);
+							break;
+						} elseif (ST_COLON == $id) {
+							break;
+						}
+					}
+					break;
+				default:
+					if (substr_count($text, $this->new_line) > 0 && !$this->is_token(array($close_token))) {
+						$text = str_replace($this->new_line, $this->new_line.$this->get_indent(), $text);
+					} elseif (substr_count($text, $this->new_line) > 0 && $this->is_token(array($close_token))) {
+						$this->set_indent(-1);
+						$text = str_replace($this->new_line, $this->new_line.$this->get_indent(), $text);
+						$this->set_indent(+1);
+					}
+					$this->append_code($text, false);
+					break;
+			}
+		}
+		return $this->code;
+	}
+	private function format_for_blocks($source) {
+		return $this->format_blocks($source, T_FOR, T_ENDFOR);
+	}
+	private function format_foreach_blocks($source) {
+		return $this->format_blocks($source, T_FOREACH, T_ENDFOREACH);
+	}
+	private function format_while_blocks($source) {
+		$this->tkns = token_get_all($source);
+		$this->code = '';
+
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr       = $index;
+			switch ($id) {
+				case T_ENDWHILE:
+					$this->set_indent(-1);
+					$this->append_code($text, false);
+					break;
+				case T_WHILE:
+					$this->append_code($text, false);
+					while (list($index, $token) = each($this->tkns)) {
+						list($id, $text) = $this->get_token($token);
+						$this->ptr       = $index;
+						$this->append_code($text, false);
+						if (ST_CURLY_OPEN == $id) {
+							break;
+						} elseif (ST_SEMI_COLON == $id) {
+							break;
+						} elseif (ST_COLON == $id) {
+							$this->set_indent(+1);
+							break;
+						}
+					}
+					break;
+				default:
+					if (substr_count($text, $this->new_line) > 0 && !$this->is_token(array(T_ENDWHILE))) {
+						$text = str_replace($this->new_line, $this->new_line.$this->get_indent(), $text);
+					} elseif (substr_count($text, $this->new_line) > 0 && $this->is_token(array(T_ENDWHILE))) {
+						$this->set_indent(-1);
+						$text = str_replace($this->new_line, $this->new_line.$this->get_indent(), $text);
+						$this->set_indent(+1);
+					}
+					$this->append_code($text, false);
+					break;
+			}
+		}
+		return $this->code;
+	}
+}
+
+final class ReindentIfColonBlocks extends FormatterPass {
+	public function format($source) {
+		$this->tkns = token_get_all($source);
+		$this->code = '';
+
+		while (list($index, $token) = each($this->tkns)) {
+			list($id, $text) = $this->get_token($token);
+			$this->ptr       = $index;
+			switch ($id) {
+				case T_ENDIF:
+					$this->set_indent(-1);
+					$this->append_code($text, false);
+					break;
+				case T_ELSE:
+				case T_ELSEIF:
+					$this->set_indent(-1);
+				case T_IF:
+					$this->append_code($text, false);
+					while (list($index, $token) = each($this->tkns)) {
+						list($id, $text) = $this->get_token($token);
+						$this->ptr       = $index;
+						$this->append_code($text, false);
+						if (ST_CURLY_OPEN == $id) {
+							break;
+						} elseif (ST_COLON == $id && !$this->is_token(array(T_CLOSE_TAG))) {
+							$this->set_indent(+1);
+							break;
+						} elseif (ST_COLON == $id) {
+							break;
+						}
+					}
+					break;
+				default:
+					if (substr_count($text, $this->new_line) > 0 && !$this->is_token(array(T_ENDIF, T_ELSE, T_ELSEIF))) {
+						$text = str_replace($this->new_line, $this->new_line.$this->get_indent(), $text);
+					} elseif (substr_count($text, $this->new_line) > 0 && $this->is_token(array(T_ENDIF, T_ELSE, T_ELSEIF))) {
+						$this->set_indent(-1);
+						$text = str_replace($this->new_line, $this->new_line.$this->get_indent(), $text);
+						$this->set_indent(+1);
+					}
+					$this->append_code($text, false);
+					break;
+			}
+		}
+		return $this->code;
+	}
+}
 final class ReindentObjOps extends FormatterPass {
 	const ALIGNABLE_OBJOP = "\x2 OBJOP%d \x3";
 	public function format($source) {
@@ -1181,6 +1334,7 @@ final class ResizeSpaces extends FormatterPass {
 				case T_SWITCH:
 				case T_TRY:
 				case ST_COMMA:
+				case T_CLONE:
 					$this->append_code($text.$this->get_space(), false);
 					break;
 				case T_EXTENDS:
@@ -1213,11 +1367,23 @@ final class ResizeSpaces extends FormatterPass {
 				case ST_IS_GREATER:
 				case ST_IS_SMALLER:
 				case T_AS:
-				case T_ELSE:
-				case T_ELSEIF:
 				case ST_EQUAL:
 				case T_CATCH:
 					$this->append_code($this->get_space().$text.$this->get_space(), false);
+					break;
+				case T_ELSEIF:
+					if (!$this->is_token(ST_CURLY_CLOSE, true)) {
+						$this->append_code($text.$this->get_space(), false);
+					} else {
+						$this->append_code($this->get_space().$text.$this->get_space(), false);
+					}
+					break;
+				case T_ELSE:
+					if (!$this->is_token(ST_CURLY_CLOSE, true)) {
+						$this->append_code($text, false);
+					} else {
+						$this->append_code($this->get_space().$text.$this->get_space(), false);
+					}
 					break;
 				case T_ARRAY_CAST:
 				case T_BOOL_CAST:
@@ -1721,6 +1887,8 @@ if (!isset($testEnv)) {
 	$fmt->addPass(new ResizeSpaces());
 	$fmt->addPass(new Reindent());
 	$fmt->addPass(new ReindentColonBlocks());
+	$fmt->addPass(new ReindentLoopColonBlocks());
+	$fmt->addPass(new ReindentIfColonBlocks());
 	$fmt->addPass(new ReindentObjOps());
 	$fmt->addPass(new OrderUseClauses());
 	$fmt->addPass(new EliminateDuplicatedEmptyLines());
