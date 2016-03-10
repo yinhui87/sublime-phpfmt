e069db7d746d60fc8645a770551305754a572387
Shaked Klein Orbach <shaked.klein@researchgate.net>
2016-03-10 12:55:08 +0100

    fix fmt.phar path for README generator

--- generateReadme.php
+++ generateReadme.php
@@ -3,7 +3,8 @@ $str = file_get_contents(__DIR__ . '/README.src.md');
 
 $commands = json_decode(file_get_contents(__DIR__ . '/Default.sublime-commands'), true);
 
-$passes = explode(PHP_EOL, trim(sprintf(`php %s/fmt.phar --list-simple`, __DIR__)));
+$cmd = sprintf("php %s/fmt.phar --list-simple", __DIR__);
+$passes = explode(PHP_EOL, trim(`$cmd`));
 
 $strCommands = implode(PHP_EOL,
     array_map(function ($v) {
